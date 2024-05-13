a136409614e208b52f526d7fa1f35ffddd1a3b34
ramalhocsjulia <julia.souza@ee.ufcg.edu.br>
2024-05-13 19:33:19 -0300

    Final version

--- src/main.c
+++ src/main.c
@@ -4,90 +4,92 @@
 #include "task.h"
 #include "semphr.h"
 
-SemaphoreHandle_t semaphore;
-SemaphoreHandle_t mutex_e_o; // Mutex para fila leste para oeste
-SemaphoreHandle_t mutex_o_e; // Mutex para fila oeste para leste
-SemaphoreHandle_t cancela;
+SemaphoreHandle_t semaphore; // Semáforo para controlar a abertura e o fechamento da cancela
+SemaphoreHandle_t mutex_e_o; // Mutex para fila de trens no sentido leste para oeste 
+SemaphoreHandle_t mutex_o_e; // Mutex para fila de trens no sentido oeste para leste
 
-int fila_e_o = 0; // Fila leste para oeste
-int fila_o_e = 0; // Fila oeste para leste
+int fila_e_o = 0; // Fila de trens no sentido leste para oeste 
+int fila_o_e = 0; // Fila de trens no sentido oeste para leste
 
+int trem_id = 1; // ID do trem
+
+// Função para o comportamento da task trem - sentido oeste para leste
 void trem_o_e(void *pvParameters)
 {
     int id = *((int *)pvParameters);
-
     while (1)
     {
         // Determina a mensagem de direção baseada na direção do trem
         char *mensagem_direcao = "oeste para leste";
 
-        printf("Trem %d se aproximando do cruzamento (%s).\n", id, mensagem_direcao);
+        printf("Trem %d se aproximando do cruzamento (%s).\n", trem_id, mensagem_direcao);
 
-        xSemaphoreTake(mutex_o_e, portMAX_DELAY);
-        fila_o_e++;
+        xSemaphoreTake(mutex_o_e, portMAX_DELAY); // Adquire o mutex para acessar a fila
+        fila_o_e++; 
         if (fila_o_e == 1 && fila_e_o == 0)
         {
             printf("Semaforo fica vermelho -- Cancela fechada\n");
-            xSemaphoreTake(semaphore, portMAX_DELAY);
+            xSemaphoreTake(semaphore, portMAX_DELAY); // Fecha a cancela se um trem se aproxima enquanto não há outros trens
         }
-        xSemaphoreGive(mutex_o_e);
+        xSemaphoreGive(mutex_o_e); // Libera o mutex para a fila
 
-        printf("Trem %d cruzando o cruzamento (%s).\n", id, mensagem_direcao);
+        printf("Trem %d atravessando o cruzamento (%s).\n", trem_id, mensagem_direcao);
         vTaskDelay(7); // Simula o tempo de cruzamento
-        printf("Trem %d passou pelo cruzamento (%s).\n", id, mensagem_direcao);
+        printf("Trem %d passou pelo cruzamento (%s).\n", trem_id, mensagem_direcao);
 
-        xSemaphoreTake(mutex_o_e, portMAX_DELAY);
-        fila_o_e--;
+        xSemaphoreTake(mutex_o_e, portMAX_DELAY); // Adquire o mutex para acessar a fila
+        fila_o_e--; 
         if (fila_e_o == 0 && fila_o_e == 0)
         {
             printf("Semaforo fica verde -- Cancela aberta\n");
-            xSemaphoreGive(semaphore);
+            xSemaphoreGive(semaphore); // Abre a cancela se não há trens
         }
-        xSemaphoreGive(mutex_o_e);
+        xSemaphoreGive(mutex_o_e); // Libera o mutex para a fila
 
-        id = id + 1;
+        trem_id++;
         vTaskDelay((rand() % 125 + 200) / portTICK_PERIOD_MS); // Simula o tempo entre os trens
     }
 }
 
+// Função para o comportamento da task trem - sentido leste para oeste
 void trem_e_o(void *pvParameters)
 {
     int id = *((int *)pvParameters);
-
     while (1)
     {
         // Determina a mensagem de direção baseada na direção do trem
         char *mensagem_direcao = "leste para oeste";
 
-        printf("Trem %d se aproximando do cruzamento (%s).\n", id, mensagem_direcao);
+        printf("Trem %d se aproximando do cruzamento (%s).\n", trem_id, mensagem_direcao);
 
-        xSemaphoreTake(mutex_e_o, portMAX_DELAY);
-        fila_e_o++;
+        xSemaphoreTake(mutex_e_o, portMAX_DELAY); // Adquire o mutex para acessar a fila
+        fila_e_o++; 
         if (fila_e_o == 1 && fila_o_e == 0)
         {
-            printf("Semaforo fica vermelho -- Cancela fechada\n");
+            printf("Semaforo fica vermelho -- Cancela fechada\n"); // Fecha a cancela se um trem se aproxima enquanto não há outros trens
             xSemaphoreTake(semaphore, portMAX_DELAY);
         }
-        xSemaphoreGive(mutex_e_o);
+        xSemaphoreGive(mutex_e_o); // Libera o mutex
 
-        printf("Trem %d cruzando o cruzamento (%s).\n", id, mensagem_direcao);
+        printf("Trem %d atravessando o cruzamento (%s).\n", trem_id, mensagem_direcao);
         vTaskDelay(7); // Simula o tempo de cruzamento
-        printf("Trem %d passou pelo cruzamento (%s).\n", id, mensagem_direcao);
+        printf("Trem %d passou pelo cruzamento (%s).\n", trem_id, mensagem_direcao);
 
-        xSemaphoreTake(mutex_e_o, portMAX_DELAY);
-        fila_e_o--;
+        xSemaphoreTake(mutex_e_o, portMAX_DELAY); // Adquire o mutex para acessar a fila
+        fila_e_o--; 
         if (fila_e_o == 0 && fila_o_e == 0)
         {
             printf("Semaforo fica verde -- Cancela aberta\n");
-            xSemaphoreGive(semaphore);
+            xSemaphoreGive(semaphore); //Abre a cancela se não há trens
         }
-        xSemaphoreGive(mutex_e_o);
+        xSemaphoreGive(mutex_e_o); // Libera o mutex
 
-        id = id + 1;
+        trem_id++;
         vTaskDelay((rand() % 250 + 200) / portTICK_PERIOD_MS); // Simula o tempo entre os trens
     }
 }
 
+// Função para o comportamento da task carro
 void carro(void *pvParameters)
 {
     int id = *((int *)pvParameters);
@@ -98,9 +100,9 @@ void carro(void *pvParameters)
         printf("Carro %d chegou na cancela.\n", id);
         if (xSemaphoreTake(semaphore, pdMS_TO_TICKS(100)) == pdTRUE)
         {
-            printf("Carro %d passando.\n", id);
-            vTaskDelay(30); // Simula o tempo de travessia
-            printf("Carro %d passou .\n", id);
+            printf("Carro %d atravessando o cruzamento.\n", id);
+            vTaskDelay(30); // Simula o tempo de cruzamento
+            printf("Carro %d passou pelo cruzamento.\n", id);
             xSemaphoreGive(semaphore); // Libera o semáforo da cancela após o carro passar
         }
         else
@@ -115,28 +117,21 @@ void carro(void *pvParameters)
 
 int main()
 {
-
     srand(time(NULL));
 
+    // Criação dos semáforos
     semaphore = xSemaphoreCreateMutex();
     mutex_e_o = xSemaphoreCreateMutex();
     mutex_o_e = xSemaphoreCreateMutex();
 
-    // Criando threads para os trens
-    // for (int i = 0; i < 2; ++i) {
+    // Criação de tasks para os trens e carros
     int *id = (int *)malloc(sizeof(int));
     *id = 1;
     xTaskCreate(trem_o_e, "Trem", 2048, (void *)id, 1, NULL);
     xTaskCreate(trem_e_o, "Trem", 2048, (void *)id, 1, NULL);
-    // }
-
-    // Criando threads para os carros
-    //  for (int i = 0; i < 2; ++i) {
-    //  int *id = (int *)malloc(sizeof(int));
-    //*id = i + 1;
     xTaskCreate(carro, "Carro", 2048, (void *)id, 1, NULL);
-    //}
 
+    // Inicia o escalonador de tarefas
     vTaskStartScheduler();
 
     return 0;
